444e85ee070bec1763ba9638a01582d49f06342e
John Lettman <the@johnlettman.com>
2025-04-19 10:07:22 -0400

    feat(ueye-sys): add stop_live_video.rs

--- ueye-sys/src/constants/live_freeze.rs
+++ ueye-sys/src/constants/live_freeze.rs
@@ -2,8 +2,13 @@ use crate::types::UINT;
 
 pub const IS_GET_LIVE: UINT = 0x8000;
 
+/// The function waits.
 pub const IS_WAIT: UINT = 0x0001;
+
+/// The function returns immediately.
 pub const IS_DONT_WAIT: UINT = 0x0000;
+
+/// Digitizing is stopped immediately.
 pub const IS_FORCE_VIDEO_STOP: UINT = 0x4000;
 pub const IS_FORCE_VIDEO_START: UINT = 0x4000;
 pub const IS_USE_NEXT_MEM: UINT = 0x8000;
